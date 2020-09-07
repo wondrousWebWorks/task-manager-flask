70c7a53015e8b902e5d2ebc33937278f25c64ecf
Johann Alberts <alberts.johann@gmail.com>
2020-09-07 14:20:33 +0100

    Added registration page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists('env.py'):
     import env
@@ -19,10 +20,14 @@ MONGO = PyMongo(APP)
 @APP.route('/get_tasks')
 def get_tasks():
     """Display the Task Manager page"""
-
     tasks = MONGO.db.tasks.find()
-
     return render_template('index.html', tasks=tasks)
+
+
+@APP.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     APP.run(host=os.environ.get('IP'),
