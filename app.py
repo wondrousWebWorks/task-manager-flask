331b7352ad0625ced52577708116dc370f6d0251
Johann Alberts <alberts.johann@gmail.com>
2020-09-11 10:42:06 +0100

    Add Session User Profile

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def register():
         # put the new user into 'session cookie'
         session['user'] = request.form.get('username').lower()
         flash('Registration successfull')
+        return redirect(url_for('profile', username=session['user']))
+
     return render_template('register.html')
 
 
@@ -59,7 +61,10 @@ def login():
             if check_password_hash(
                 existing_user['password'], request.form.get('password')):
                     session['user'] = request.form.get('username').lower()
-                    flash('Welcome, {}'.format(request.form.get('username')))
+                    flash('Welcome, {}'.format(
+                        request.form.get('username')))
+                    return redirect(url_for(
+                        'profile', username=session['user']))
             else:
                 flash('Incorrect Username and/or Password')
                 return redirect(url_for('login'))
@@ -70,6 +75,14 @@ def login():
                 return redirect(url_for('login'))
 
     return render_template('login.html')
+
+
+@APP.route('/profile<username>', methods=['GET', 'POST'])
+def profile(username):
+    #grab the session user's name from the db'
+    username = MONGO.db.users.find_one(
+        {'username': session['user']})['username']
+    return render_template('profile.html', username=username)
 
 
 if __name__ == '__main__':
