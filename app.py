52cf6ad371463096f7b5c7d626cfb462d79899dc
Johann Alberts <alberts.johann@gmail.com>
2020-09-11 10:06:54 +0100

    Added login functionality

--- app.py
+++ app.py
@@ -47,6 +47,31 @@ def register():
     return render_template('register.html')
 
 
+@APP.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # check if userna,e exists in db
+        existing_user = MONGO.db.users.find_one(
+            {"username": request.form.get('username').lower()})
+
+        if existing_user:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_user['password'], request.form.get('password')):
+                    session['user'] = request.form.get('username').lower()
+                    flash('Welcome, {}'.format(request.form.get('username')))
+            else:
+                flash('Incorrect Username and/or Password')
+                return redirect(url_for('login'))
+
+        else:
+            #username doesn't exist
+                flash('Incorrect Username and/or Password')
+                return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     APP.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
