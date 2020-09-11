a7abf9ff0996110972fd3f2ddef04b30af443dbf
Johann Alberts <alberts.johann@gmail.com>
2020-09-11 11:23:07 +0100

    Add logout functionality

--- app.py
+++ app.py
@@ -82,7 +82,19 @@ def profile(username):
     #grab the session user's name from the db'
     username = MONGO.db.users.find_one(
         {'username': session['user']})['username']
-    return render_template('profile.html', username=username)
+
+    if session['user']:
+        return render_template('profile.html', username=username)
+
+    return redirect(url_for('login'))
+
+
+@APP.route('/logout')
+def logout():
+    # remove user from session cookies
+    flash('You have been logged out')
+    session.pop('user')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
