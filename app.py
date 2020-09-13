2aa62a0298a1afa80095eeed09afb16e7b756eaf
Johann Alberts <alberts.johann@gmail.com>
2020-09-13 17:16:55 +0100

    Create Add Task Template

--- app.py
+++ app.py
@@ -97,6 +97,11 @@ def logout():
     return redirect(url_for('login'))
 
 
+@APP.route('/add_task')
+def add_task():
+    return render_template('add_task.html')
+
+
 if __name__ == '__main__':
     APP.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
