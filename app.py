45ccef34cf15b337e799272bec44933d82e1ff38
Johann Alberts <alberts.johann@gmail.com>
2020-09-21 10:54:47 +0100

    Add Delete Category functionality

--- app.py
+++ app.py
@@ -177,6 +177,13 @@ def edit_category(category_id):
     return render_template('edit_category.html', category=category)
 
 
+@APP.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    MONGO.db.categories.remove({'_id': ObjectId(category_id)})
+    flash('Category Successfully Deleted')
+    return redirect(url_for('get_categories'))
+
+
 
 if __name__ == '__main__':
     APP.run(host=os.environ.get('IP'),
